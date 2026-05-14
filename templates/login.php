8fc01e85c01138831a36e95700d7c2f446f75df1
sylthalith <sylthalith@gmail.com>
2026-05-14 16:32:42 +0500

    Экранирование полей ввода

--- templates/login.php
+++ templates/login.php
@@ -3,7 +3,7 @@
         <input type="hidden" name="csrf_token" value="<?= csrf_token() ?>">
         <div class="input-block">
             <label for="email">Почта</label>
-            <input id="email" type="email" name="email" value="<?= $email ?? '' ?>">
+            <input id="email" type="email" name="email" value="<?= htmlspecialchars($email ?? '', ENT_QUOTES, 'UTF-8') ?>">
             <?php if (isset($errors['email'])) : ?>
                 <?php template('partials/errors', ['errors' => $errors['email']]) ?>
             <?php endif ?>
